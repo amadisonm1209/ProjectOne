4f674b34473200b217399bf865d48da173d7cc0d
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-06-06 15:20:16 -0600

    moon phases are pulling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,13 @@ const weatherKey = "88b9c8ef1d303abfad87f0e3796672aa";
 var incidentTime = [];
 var incidentLat = [];
 var incidentLong = [];
-var timeConvertedUnix = moment(incidentTime).format("X");
+var timeConvertedUnixArray = [];
+var moonPhaseNum = [];
+var newMoon = [];
+var firstQuarterMoon = [];
+var fullMoon = [];
+var thirdQuarterMoon = [];
+var crimeSummary = [];
 
 var map;
 
@@ -63,106 +69,102 @@ submitButton.on("click", function (event) {
 
     // grabbing limit value
     var limit = $("#userEntrySelection").val();
-    var place 
-    
-     // grabbing value of city selected
-     city = $("#userCitySelection option:selected").val()
-      
-     // returning coordinates and initiating map for each city
-     switch (city) {
-         case "Option 1":
-             initMap(Chandler.lat, Chandler.lng);
-             place = Chandler.id;
-             break;
-         case "Option 2":
-             initMap(Detroit.lat, Detroit.lng);
-             place = Detroit.id;
-             break;
-         case "Option 3":
-             initMap(Mesa.lat, Mesa.lng);
-             place= Mesa.id;
-             break;
-         case "Option 4":
-             initMap(SanFrancisco.lat, SanFrancisco.lng);
-             place = SanFrancisco.id;
-             break;
-         case "Option 5":
-             initMap(Scottsdale.lat, Scottsdale.lng);
-             place = Scottsdale.id;
-             break;
-         case "Option 6":
-             initMap(Seattle.lat, Seattle.lng);
-             place = Seattle.id;
-     };
+    var place
+
+    // grabbing value of city selected
+    city = $("#userCitySelection option:selected").val()
+
+    // returning coordinates and initiating map for each city
+    switch (city) {
+        case "Option 1":
+            initMap(Chandler.lat, Chandler.lng);
+            place = Chandler.id;
+            break;
+        case "Option 2":
+            initMap(Detroit.lat, Detroit.lng);
+            place = Detroit.id;
+            break;
+        case "Option 3":
+            initMap(Mesa.lat, Mesa.lng);
+            place = Mesa.id;
+            break;
+        case "Option 4":
+            initMap(SanFrancisco.lat, SanFrancisco.lng);
+            place = SanFrancisco.id;
+            break;
+        case "Option 5":
+            initMap(Scottsdale.lat, Scottsdale.lng);
+            place = Scottsdale.id;
+            break;
+        case "Option 6":
+            initMap(Seattle.lat, Seattle.lng);
+            place = Seattle.id;
+    };
 
     // ajax call to municipal site
     $.ajax({
-        url: "https://municipal.systems/v1/places/" + place + "/dataTypes/crime/data?key=" + mapKey + "&limit=" + limit,
+        url: "https://municipal.systems/v1/places/" + place + "/dataTypes/crime/data?key=" + mapKey + "&limit=" + limit + "&offset=10",
         method: "GET"
     }).then(function (crimeResponse) {
         console.log(crimeResponse);
-
         for (var i = 0; i < crimeResponse.results.length; i++) {
+
+            var timeConvertedUnix = moment(incidentTime[i]).format("X");
+            timeConvertedUnixArray.push(timeConvertedUnix);
             incidentLong.push(crimeResponse.results[i].data.location.coordinates[0].toFixed(4));
             incidentLat.push(crimeResponse.results[i].data.location.coordinates[1].toFixed(4));
             incidentTime.push(crimeResponse.results[i].data.startedAt);
+            crimeSummary.push(crimeResponse.results[i].data.type);
+
             var coords = crimeResponse.results[i].data.location.coordinates;
-            var latLng = new google.maps.LatLng(coords[1],coords[0]);
+            var latLng = new google.maps.LatLng(coords[1], coords[0]);
             var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
+                position: latLng,
+                map: map
 
-            
-
+            });
         };
-    }).then(function (result) {
-        weatherResponse();
-    });
-
-    //ajax call using the crime data to the weather api
-    function weatherResponse() {
-
+    }).then(function weatherResponse() {
         for (var i = 0; i < incidentLat.length; i++) {
             $.ajax({
-                url: "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/88b9c8ef1d303abfad87f0e3796672aa/" + incidentLat[i] + "," + incidentLong[i] + "," + timeConvertedUnix[i],
+                url: "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/88b9c8ef1d303abfad87f0e3796672aa/" + incidentLat[i] + "," + incidentLong[i] + "," + timeConvertedUnixArray[i],
                 method: "GET"
             }).then(function (weatherResponse) {
-
                 var weatherSummary = weatherResponse.currently.summary;
                 //display weather summary 
-                var moonPhaseNum = weatherResponse.daily.data[0].moonPhase;
-
-                if (moonPhaseNum >= 0 && moonPhaseNum < 0.2) {
-
-                    //display "New Moon"
-                } else if (moonPhaseNum >= 0.2 && moonPhaseNum < 0.4) {
-                    //display "First Quarter Moon"
-                } else if (moonPhaseNum >= 0.4 && moonPhaseNum < 0.7) {
-                    //display "Full Moon"
-                } else if (moonPhaseNum >= 0.7 && moonPhaseNum <= 1.00) {
-                    //display "Third Quarter Moon"
-                    // alert("I'm a moon");
-                };
-            })
-        }
-
-    };
-     
+                moonPhaseNum.push(weatherResponse.daily.data[0].moonPhase);
+                checkMoonPhase(moonPhaseNum);
+            });
+        };
+    });
 });
 
+
 //need to filter through results using moon phase set above
-    $(".moon-image").on("click", function () {
-        var moonPhase = $(this).data("value");
-        console.log(moonPhase);
+$(".moon-image").on("click", function () {
+    var moonPhase = $(this).data("value");
+    console.log(moonPhase);
+});
+
+
+// function to display map
+
+function initMap(latitude, longitude) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: latitude, lng: longitude },
+        zoom: 10
     });
+};
 
-   
-    // function to display map
 
-    function initMap(latitude, longitude) {
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: latitude, lng: longitude },
-            zoom: 10
-        });
+function checkMoonPhase(moonPhaseNum) {
+    if (moonPhaseNum >= 0 && moonPhaseNum < 0.2) {
+        newMoon.push(crimeSummary);
+    } else if (moonPhaseNum >= 0.2 && moonPhaseNum < 0.4) {
+        firstQuarterMoon.push(crimeSummary);
+    } else if (moonPhaseNum >= 0.4 && moonPhaseNum < 0.7) {
+        fullMoon.push(crimeSummary);
+    } else if (moonPhaseNum >= 0.7 && moonPhaseNum <= 1.00) {
+        thirdQuarterMoon.push(crimeSummary);
     };
+}
